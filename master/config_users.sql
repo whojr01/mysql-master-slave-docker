a8d21b259c036cd70449fe7d79f3a34de3e40146
William Oliver <boliver@whojr.com>
2021-12-09 11:14:17 -0500

    More tweaks for replication and consolidated user creation

--- master/config_users.sql
+++ master/config_users.sql
@@ -1,7 +1,8 @@
 /*
-  * config_master
+  * config_users
   * Sets the root password after datadir is created
   * Creates mysql user with passowrd mysql-pass
+  * Creates replication user with password replication
   * Grants privileges to mysql user
 */
 
@@ -9,4 +10,8 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY 'mysql-pass';
 
 CREATE USER 'mysql'@'localhost' IDENTIFIED BY 'mysql-pass';
 GRANT CREATE, INSERT, DROP, UPDATE, SELECT,SHUTDOWN ON *.* TO 'mysql'@'localhost';
+
+CREATE USER 'replication'@'localhost' IDENTIFIED BY 'replication';
+GRANT REPLICATION SLAVE ON *.* TO 'replication'@'localhost';
+
 flush privileges;
